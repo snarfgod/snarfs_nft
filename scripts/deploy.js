38acf1b433111e860ce5022235f45e65f04a275a
snarfgod <dave.ports.1@gmail.com>
2023-07-15 22:12:21 -0400

    updated deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,25 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const NAME = 'Snarfcoin'
+    const SYMBOL = 'SNARF'
+    const MAX_SUPPLY = '1000000000000000000000000'
+    const PRICE = ethers.utils.parseUnits('.025', 'ether')
+    
+    const Token = await hre.ethers.getContractFactory("Token");
+    let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+    await token.deployed();
+    console.log("Token deployed to:", token.address);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+    const Crowdsale = await hre.ethers.getContractFactory("Crowdsale");
+    let crowdsale = await Crowdsale.deploy(token.address, PRICE, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'));
+    await crowdsale.deployed();
+    console.log("Crowdsale deployed to:", crowdsale.address);
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    const transaction = await token.transfer(crowdsale.address, ethers.utils.parseUnits('1000000', 'ether'));
+    await transaction.wait();
+    console.log("Transfered tokens to crowdsale");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
